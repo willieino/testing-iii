777c01425cde4ca1e57da6e8b6757ec386a9bef7
Willie Yturralde <willieino@hotmail.com>
2019-02-05 00:55:24 -0800

    created 7 tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,35 @@
 // Test away
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+import Dashboard from './Dashboard';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+
+describe('renders the Dashboard without crashing', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Dashboard />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  
+  }) 
+
+ describe('the Button components', () => {
+  test('the button functions properly', () => {
+   /*  const component = render(<Display />); */
+    const component = render(<Dashboard />);
+     const component2 = render(<Controls locked="false"/>);  
+    const button = component2.getByText(/Lock Gate/i);
+    fireEvent.click(button);
+    const component3 = render(<Display />);
+    component3.getByText(/Locked/i);
+   
+   /*  const balls = component.getByTestId('balls')
+    expect(balls).toHaveTextContent('1'); */
+  })
+
+}); 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,29 @@
 // Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './display';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+describe('renders the Display without crashing', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Display />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+  describe('the Lock Display component', () => {
+    test('the lock will display the correct led', () => {
+      const component = render(<Display locked="false" />);
+      component.getByText(/Locked/i);
+    });
+  });
+
+  describe('the Gate Display component', () => {
+    test('the gate will display the correct led', () => {
+      const component = render(<Display closed="false" />);
+      component.getByText(/Closed/i);
+    });
+  });
